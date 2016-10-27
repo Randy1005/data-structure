66940d62406e4265f512fbd6a0faa3caa1a4f2dc
Randy1005 <randychang.10@gmail.com>
2016-10-28 02:37:49 +0800

    Input complete

--- hw2/hw2-1.c
+++ hw2/hw2-1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int value(int n, int a, int b, int i, int j, int e[]);
+
 int main()
 {
 	printf("Please input the file name: ");
@@ -9,25 +11,62 @@ int main()
 	//printf("%s",inputFile);
 	
 	FILE *ifp = fopen(inputFile,"r");
-	int matrixDim;
-	int matrix[matrixDim][matrixDim];
-	int i,j=0;
+	int matrixDim = 0;
+	int i,j = 0;
+	int lineCnt = 1;
+
+	/*get the first line, which is the matrixDimension*/
+	
 	while(!feof(ifp))
 	{
-		fscanf(ifp,"%d\n",&matrixDim);
+		if(lineCnt == 1)
+		{
+			fscanf(ifp,"%d\n",&matrixDim);
+			lineCnt++;
+		}
+		else
+			break;	
+	}
+
+	/*DEBUG: matrixDim*/
+	//printf("%d\n",matrixDim);
+	
+	/*create matrix*/
+	int matrix[matrixDim][matrixDim];
+	
+	/*
+	char buffer[100];
+	fgets(buffer,100,ifp);
+	printf("%s",buffer);
+	*/
+	
+	while(!feof(ifp))
+	{
 		for(i=0;i<matrixDim;i++)
 		{
 			for(j=0;j<matrixDim;j++)
-				fscanf(ifp,"%d ",&matrix[i][j]); 
+				fscanf(ifp,"%d",&matrix[i][j]);
 		}
+		/*
+		if(feof(ifp))
+			printf("END OF FILE\n");
+		*/
 	}
-
+		
+	/*DEBUG: matrix*/
+	/*
 	for(i=0;i<matrixDim;i++)
-		for(i=0;i<matrixDim;i++)
+	{
+		for(j=0;j<matrixDim;j++)
 			printf("%d ",matrix[i][j]);
+		printf("\n");
+	}
+	*/
+	
+	
+}
 
-
-
-
+int value(int n, int a, int b, int i, int j, int e[])
+{
 
 }
