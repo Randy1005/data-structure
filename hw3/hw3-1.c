6dd7411ab9b48fe36470ee5f32f517e417a5a34c
Randy1005 <randychang.10@gmail.com>
2016-11-08 00:25:32 +0800

    finished 'one solution condition'

--- hw3/hw3-1.c
+++ hw3/hw3-1.c
@@ -2,6 +2,61 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/*std=c99 compile*/
+/*needs to pass at least second element to 2D array as fucntion parameter*/
+
+typedef struct {
+	int x;
+	int y;
+}Point;
+
+void printSol(int M,int N,char sol[][N])
+{
+	int i,j;
+	for(i=0;i<M;i++)
+	{	
+		for(j=0;j<N;j++)
+			printf("%c",sol[i][j]);
+		printf("\n");
+	}
+}
+
+
+/*
+bool isSafe(int x,int y,int M,int N)
+{
+	if(x >= 0 && x < M && y >= 0 && y < N)
+		return true;
+}
+*/
+
+bool visit(int N,char maze[][N],int i,int j)
+{
+	if(maze[i][j] == 'd') //reaches destination
+		return true;
+
+	else if(maze[i][j] == 's' || maze[i][j] == '0') //a valid path
+	{
+			maze[i][j] = '*';
+
+		if(visit(N,maze,i-1,j) || visit(N,maze,i+1,j) || visit(N,maze,i,j-1) || visit(N,maze,i,j+1) || visit(N,maze,i-1,j+1) || visit(N,maze,i-1,j-1) || visit(N,maze,i+1,j+1) || visit(N,maze,i+1,j-1))
+			return true; //try 7 directions
+		 
+		else //if not valid
+		{
+				maze[i][j] = '0'; //unmark
+				return false;
+		}
+	}
+
+	else //encounters wall '1'
+		return false; 
+		
+}
+
+
+
+
 int main()
 {
 	FILE *ifp;
@@ -46,7 +101,7 @@ int main()
 	int i,j;
 
 	/*initialize maze array*/
-	
+
 	for(i=0;i<M;i++)
 		for(j=0;j<N;j++)
 			maze[i][j] = '0';
@@ -75,21 +130,34 @@ int main()
 	}
 	fclose(ifp);
 	
-	
-	
-	/*
+	/*Here we start solving the maze*/
+
+	/*first get the starting position*/
+	Point startPt;
 	for(i=0;i<M;i++)
-	{
 		for(j=0;j<N;j++)
-			printf("%c",maze[i][j]);
-		printf("\n");
+		{
+			if(maze[i][j] == 's')
+			{
+				startPt.x = i;
+				startPt.y = j;
+			}
+		}
+	
+	/*set up initial postion and start solving the maze*/
+	if(visit(N,maze,startPt.x,startPt.y))
+	{
+		/*changed 's' to '*' in visit accidentally, change back to 's'*/
+		maze[startPt.x][startPt.y] = 's';
+		printSol(M,N,maze);
 	}
-	*/
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 
 
 
