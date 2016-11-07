22759c106677f56fe13681cbe6c0ef9baeaf9690
Randy1005 <randychang.10@gmail.com>
2016-11-07 20:14:59 +0800

    why 12 newline characters?!

--- hw3/hw3-1.c
+++ hw3/hw3-1.c
@@ -7,23 +7,89 @@ int main()
 	FILE *ifp;
 	FILE *ofp;
 	ifp = fopen("in.txt","r");
-	
+
 	/*to get M and N*/
 	int M = 0;
 	int N = 0;
+
+	char temp;
+	/*get N*/
 	while(!feof(ifp))
 	{
-		bool M_flag = false; //count one line is enough, after we get M, set to 'true'
-		char temp;
-		fscanf(ifp,"%c",&temp);
-		if(temp != '\n' && M_flag == false)
-			M++;
+		if((temp = fgetc(ifp)) == '\n')
+		{
+			N--;
+			break;
+		}
 		else
-			M_flag = true;
+			N++;
 	}
 
+	/*get M*/
+	while(!feof(ifp))
+	{
+		if((temp = fgetc(ifp)) == '\n')
+			M++;
+
+		else if((temp = fgetc(ifp)) == EOF)
+		{
+			M++;
+		}
+		else
+			;
+
+	}
+
+
+	/*create maze array*/
+	char maze[M][N];
+	int i,j;
+
+	/*initialize maze array*/
+	
+	for(i=0;i<M;i++)
+		for(j=0;j<N;j++)
+			maze[i][j] = '0';
+
+	/*It won't stop emitting EOF signal, I have to close file and open again*/
 	fclose(ifp);
-	printf("%d\n",M);
+	ifp = fopen("in.txt","r");
+
+
+	/*read in the maze*/
+	//printf("M:%d N:%d\n",M,N);
+	i = 0;
+	j = 0;
+	while((temp = fgetc(ifp)) != EOF)
+	{
+		if((temp = fgetc(ifp)) != '\n')
+		{
+			printf("come on ");
+			maze[i][j] = temp;
+			j++;
+		}
+		else
+		{
+			i++;
+			j = 0;
+		}				
+	}
+	fclose(ifp);
+	
+	
+	/*
+	for(i=0;i<M;i++)
+	{
+		for(j=0;j<N;j++)
+			printf("%c",maze[i][j]);
+		printf("\n");
+	}
+	*/
+	
+	
+	
+	
+
 
 
 }
