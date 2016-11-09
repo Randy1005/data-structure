2743dcdfe7013d7d67327faa38d3dcdc7fefc63e
Randy1005 <randychang.10@gmail.com>
2016-11-09 23:18:54 +0800

    noncircular queue completed

--- hw3/nonCir_q.c
+++ hw3/nonCir_q.c
@@ -3,9 +3,10 @@
 #define MAX 4
 
 /*declare array for queue and rear, front index*/
-char queueArray[MAX] = {' ',' ',' ',' '}
-int rear = -1;
-int front = -1;
+int queueArray[MAX] = {};
+int hasValue[MAX] = {}; 
+int rear = 0;
+int front = 0;
 
 void insert();
 void delete();
@@ -13,64 +14,86 @@ void printQueue();
 
 int main()
 {
-	
-}
+	int choice;
+	while(1)
+	{
+		printf("insert(1) delete(2): ");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+				insert();
+				break;
+			case 2:
+				delete();
+				break;
 
-void chkFull()
-{
+		}
+	}
+
 
 }
 
 void insert()
 {
-	char add_element;
-	if(rear == MAX - 1)
+	if(rear == MAX -1)
 	{
-		printf("queue is full, stop inserting\n");
-	}
-	else
-	{
-		printf("Insert number: ");
-		scanf("%c",&add_element);
-		rear++; //every time I push an element rear increments
-		queueArray[rear] = add_element;
+		printf("Queue is full\n");
 		return;
 	}
+
+	int add_element;
+	printf("Insert number: ");
+	scanf("%d",&add_element);
+	queueArray[rear+1] = add_element;
+	rear++; //every time I push an element rear increments
+	hasValue[rear] = 1; //to decide whether to print out number or not, cuz queue is initialized as 0s
+
+	printQueue();
 
 }
 
 void delete()
-{
-	char delete_element;
-	if((front == -1 && rear == -1) || front > rear) //queue is already empty
+{	
+	if(front == rear)
 	{
-		printf("queue is empty, stop deleting\n");
+		front = 0;
+		rear = 0;
+		printf("Queue is empty\n");
+		return;
 	}
 
 	else
 	{
-		printf("deleted element: %c\n",queueArray[front + 1]); //pop element from front
-		front++; //increment front
-
+		printf("deleted element: %d\n",queueArray[front + 1]); //pop element from front
+		hasValue[front + 1] = 0; //set back to 0, position empty
+		front++;
 	}
+	
+	/*determine again, output front = 3 rear = 3 isn't legal*/
+	if(front == rear)
+	{
+		front = 0;
+		rear = 0;
+	}
+	printQueue();
+
 }
 
 void printQueue()
 {
 	int i;
-	if(front == -1 && rear == -1)
+	printf("Front = %d Rear = %d\n",front,rear);
+	printf("Queue: ");
+	for(i=0;i<MAX;i++)
 	{
-		printf("Queue is empty\n");
-	}
-	else if(rear == MAX -1)
-		printf("Queue is full\n");
-	else
-	{
-		printf("Front = %d Rear = %d\n",front,rear);
-		printf("Queue: ");
-		for(i=0;i<MAX;i++)
-			printf("[%c]",queueArray[i]);
+		printf("[");
 
-		printf("\n");
+		if(hasValue[i])
+			printf("%d",queueArray[i]);
+
+		printf("]");
 	}
+
+	printf("\n\n");
 }
