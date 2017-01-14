b381a9eeac222179e3c0aec3612153f988422ef0
Randy1005 <randychang.10@gmail.com>
2017-01-15 00:09:12 +0800

    hw7 bug

---
+++ hw7/hw7.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+
+/*early events time*/
+/*ee[j] = max {ee[i] + duration of <i, j>}*/
+void cal_earlyEvent(int *ee, int events, int **aoe) 
+{
+	for (int i = 0; i < events; i++) {
+		for (int j = 0; j < events; j++) {
+			if (aoe[i][j] != 0 && ee[i] + aoe[i][j] > ee[j]) {
+				ee[j] = ee[i] + aoe[i][j];
+			}
+		}
+	}
+}
+
+/*late events time*/
+/*le[j] = min {le[i] - duration of <i, j>}*/
+void cal_lateEvent(int *le, int *ee, int events, int **aoe) 
+{
+	int len = events - 1;
+	le[len] = ee[len];
+	for (int i = len; i >= 0; --i) {
+		for (int j = len; j >= 0; --j) {
+			if (aoe[i][j] != 0 && le[j] - aoe[i][j] < le[i]) {
+				le[i] = le[j] - aoe[i][j];
+			}
+		}
+	}
+}
+
+/*early and late activity time*/
+/*
+ * for an activity<k, l>
+ * e[i] = ee[k]
+ * l[i] = le[l] - act[k][l]
+ */
+void cal_eAct_lAct(int *e, int *l, int *ee, int *le, int events, int **aoe)
+{
+	int k = 0;
+	for (int i = 0; i < events; i++) {
+		for (int j = 0; j < events; j++) {
+			if (aoe[i][j] != 0) {
+				e[k] = ee[i];
+				l[k++] = le[j] - aoe[i][j];
+			}
+		}
+	}
+
+}
+
+
+int main()
+{
+	FILE *ifp = fopen("Sample_input.txt","r");
+	int event = 0;
+	int lineCnt = 1;
+	int activities = 0;
+
+	/*get the first line, which is the matrixDimension*/
+
+	while(!feof(ifp))
+	{
+		if(lineCnt == 1)
+		{
+			fscanf(ifp,"%d\n",&event);
+			lineCnt++;
+		}
+		else
+			break;	
+	}
+
+	/*create AOE matrix*/
+	int **aoe;
+
+	while(!feof(ifp))
+	{
+		for(int i=0;i<event;i++)
+		{
+			for(int j=0;j<event;j++)
+				fscanf(ifp,"%d",&aoe[i][j]);
+		}
+		/*
+		   if(feof(ifp))
+		   printf("END OF FILE\n");
+		 */
+	}
+	fclose(ifp);
+
+	/*count activities*/
+	for(int i=0;i<event;i++)
+		for(int j=0;j<event;j++)
+			if(aoe[i][j]) activities++;
+
+
+	/*calculate early event time & late event time*/
+	int eEvent[event];
+	int lEvent[event];
+
+	for(int i=0;i<event;i++)
+	{
+		eEvent[i] = 0;
+		lEvent[i] = 9999;
+	}
+	
+	cal_earlyEvent(eEvent,event,aoe);
+	cal_lateEvent(lEvent,eEvent,event,aoe);
+	int eAct[activities];
+	int lAct[activities];
+	
+	cal_eAct_lAct(eEvent,lEvent,eAct,lAct,event,aoe);
+	
+	cout << "e:" <<	endl;
+	for(int i=0;i<activities;i++)
+		cout << eAct[i] << " ";
+	cout << endl;
+
+	cout << "l:" <<endl;
+	for(int i=0;i<activities;i++)
+		cout << lAct[i] <<endl;
+	cout << endl;
+}
+
+
+
+
+
+
